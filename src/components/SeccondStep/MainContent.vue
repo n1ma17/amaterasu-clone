c53147d72a21e963fec1946b52df4dfc0a0f8d8b
n1ma17 <nimaa9217@gmail.com>
2025-03-21 17:08:09 +0330

    feat 3d model

--- src/components/SeccondStep/MainContent.vue
+++ src/components/SeccondStep/MainContent.vue
@@ -69,68 +69,82 @@
       </div>
       <div class="background-ovals">
         <div ref="oval1" class="oval1 oval-bg">
-                  <div class="wave-divider">
-          <svg
-            id="wave"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 456.7 39.9"
-            style="enable-background: new 0 0 456.7 39.9"
-            xml:space="preserve"
-          >
-            <path
-              class="st69"
-              d="M4.2,33.2c0.1-0.1,7-6.9,15.9-13.8C27.7,13.7,38.7,6,47.5,6c7.5,0,14,6.6,20.3,12.9l0.4,0.4
+          <div class="wave-divider">
+            <svg
+              id="wave"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 456.7 39.9"
+              style="enable-background: new 0 0 456.7 39.9"
+              xml:space="preserve"
+            >
+              <path
+                class="st69"
+                d="M4.2,33.2c0.1-0.1,7-6.9,15.9-13.8C27.7,13.7,38.7,6,47.5,6c7.5,0,14,6.6,20.3,12.9l0.4,0.4
 	c6.8,6.9,14.6,14.6,24.6,14.6c9.9,0,17.7-7.8,24.5-14.6l0.5-0.5C124,12.5,130.5,6,137.9,6c7.5,0,13.9,6.5,20.2,12.9l0.4,0.4
 	c6.8,6.9,14.6,14.6,24.5,14.6c10,0,17.8-7.8,24.6-14.6l0.5-0.5C214.4,12.5,220.9,6,228.4,6c7.5,0,14,6.5,20.2,12.9l0.4,0.4
 	c6.8,6.9,14.5,14.6,24.5,14.6c9.9,0,17.7-7.8,24.5-14.6l0.3-0.3c6.3-6.4,12.9-13,20.5-13c7.5,0,14.1,6.6,20.4,13l0.3,0.3
     c6.8,6.9,14.6,14.6,24.5,14.6c9.9,0,17.6-7.8,24.5-14.6l0.2-0.2C395.1,12.6,401.6,6,409.2,6c8.7,0,19.8,7.7,27.3,13.4
 	c8.9,6.8,15.9,13.7,16,13.8"
-            />
-          </svg>
-        </div>
+              />
+            </svg>
+          </div>
           How do you balance your personal ambitions with maintaining relationships?
         </div>
         <div ref="oval2" class="oval2 oval-bg">
-                  <div class="wave-divider">
-          <svg
-            id="wave"
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 456.7 39.9"
-            style="enable-background: new 0 0 456.7 39.9"
-            xml:space="preserve"
-          >
-            <path
-              class="st69"
-              d="M4.2,33.2c0.1-0.1,7-6.9,15.9-13.8C27.7,13.7,38.7,6,47.5,6c7.5,0,14,6.6,20.3,12.9l0.4,0.4
+          <div class="wave-divider">
+            <svg
+              id="wave"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              x="0px"
+              y="0px"
+              viewBox="0 0 456.7 39.9"
+              style="enable-background: new 0 0 456.7 39.9"
+              xml:space="preserve"
+            >
+              <path
+                class="st69"
+                d="M4.2,33.2c0.1-0.1,7-6.9,15.9-13.8C27.7,13.7,38.7,6,47.5,6c7.5,0,14,6.6,20.3,12.9l0.4,0.4
 	c6.8,6.9,14.6,14.6,24.6,14.6c9.9,0,17.7-7.8,24.5-14.6l0.5-0.5C124,12.5,130.5,6,137.9,6c7.5,0,13.9,6.5,20.2,12.9l0.4,0.4
 	c6.8,6.9,14.6,14.6,24.5,14.6c10,0,17.8-7.8,24.6-14.6l0.5-0.5C214.4,12.5,220.9,6,228.4,6c7.5,0,14,6.5,20.2,12.9l0.4,0.4
 	c6.8,6.9,14.5,14.6,24.5,14.6c9.9,0,17.7-7.8,24.5-14.6l0.3-0.3c6.3-6.4,12.9-13,20.5-13c7.5,0,14.1,6.6,20.4,13l0.3,0.3
     c6.8,6.9,14.6,14.6,24.5,14.6c9.9,0,17.6-7.8,24.5-14.6l0.2-0.2C395.1,12.6,401.6,6,409.2,6c8.7,0,19.8,7.7,27.3,13.4
 	c8.9,6.8,15.9,13.7,16,13.8"
-            />
-          </svg>
-        </div>
+              />
+            </svg>
+          </div>
           How do you balance your personal ambitions with maintaining relationships?
         </div>
       </div>
+    </section>
+    <section ref="section5" class="section5">
+      <div class="section5__content">
+        <div class="section5__content__header">
+          <span> Chaos </span>
+          <span> Order </span>
+        </div>
+        <span class="section5__content__text">
+          We apply non-linear dynamics to capture the complex chaos within you.
+        </span>
+      </div>
+      <div ref="threeContainer" class="three-container"></div>
     </section>
     <div ref="backgroundRef" class="background"></div>
   </div>
 </template>
 
 <script setup>
+import * as THREE from 'three'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+
 import { onBeforeUnmount, onMounted, ref } from 'vue'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import * as THREE from 'three'
 
 const logoRef = ref(null)
 const backgroundRef = ref(null)
@@ -141,7 +155,136 @@ const oval1 = ref(null)
 const oval2 = ref(null)
 const oval3 = ref(null)
 
+const threeContainer = ref(null)
+let model = null // برای ذخیره مدل سه‌بعدی
+
 onMounted(async () => {
+  gsap.fromTo(
+    '.section5',
+    { opacity: 0 },
+    {
+      opacity: 1,
+      duration: 2,
+      scrollTrigger: {
+        trigger: '.section5',
+        start: 'top 90%',
+        end: 'top 50%',
+        scrub: true,
+      },
+    },
+  )
+
+  const initThree = function () {
+    // گرفتن اندازه سه‌کانتینر
+    const containerWidth = threeContainer.value.offsetWidth
+    const containerHeight = threeContainer.value.offsetHeight
+
+    // صحنه و دوربین و رندر
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      containerWidth / containerHeight, // نسبت تصویر بر اساس ابعاد کانتینر
+      1,
+      1000,
+    )
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true }) // افزودن transparency
+    renderer.setSize(containerWidth, containerHeight) // تنظیم اندازه رندرر
+    renderer.setPixelRatio(window.devicePixelRatio) // پشتیبانی از نمایشگرهای با کیفیت بالا
+    threeContainer.value.appendChild(renderer.domElement)
+
+    // Loader برای مدل OBJ
+    const loader = new OBJLoader()
+
+    // لود کردن مدل
+    loader.load(
+      '/models/model3.obj',
+      (object) => {
+        object.position.set(0, -1.5, 0) // مرکز مدل
+        object.scale.set(0.07, 0.07, 0.07)
+        object.rotation.y = Math.PI / 2
+        scene.add(object)
+        model = object // ذخیره مدل برای تغییرات
+        let targetRotationX = 0 // مقدار هدف برای چرخش در محور X
+        let targetRotationY = Math.PI / 4 // مقدار هدف برای چرخش در محور Y (حفظ ۹۰ درجه)
+        let targetRotationZ = 0 // مقدار هدف برای چرخش در محور Z
+
+        let currentRotationX = 0 // مقدار فعلی چرخش در محور X
+        let currentRotationY = Math.PI / 4 // مقدار فعلی چرخش در محور Y
+        let currentRotationZ = 0 // مقدار فعلی چرخش در محور Z
+
+        const rotationSpeed = 0.02 // سرعت چرخش برای حرکت نرم
+
+        // تابع برای بروزرسانی چرخش مدل به صورت تدریجی
+        function updateRotation() {
+          currentRotationX += (targetRotationX - currentRotationX) * rotationSpeed
+          currentRotationY += (targetRotationY - currentRotationY) * rotationSpeed
+          currentRotationZ += (targetRotationZ - currentRotationZ) * rotationSpeed
+
+          // به‌روزرسانی چرخش مدل
+          model.rotation.x = currentRotationX
+          model.rotation.y = currentRotationY
+          model.rotation.z = currentRotationZ
+
+          // درخواست انیمیشن برای فریم بعدی
+          requestAnimationFrame(updateRotation)
+        }
+
+        // فراخوانی اولیه تابع انیمیشن
+        updateRotation()
+
+        // اضافه کردن رویداد mousemove به document برای چرخش مدل
+        document.addEventListener('mousemove', (event) => {
+          // محاسبه موقعیت موس نسبت به اندازه صفحه
+          const mouseX = (event.clientX / window.innerWidth) * 2 - 1 // موقعیت موس در محور X
+          const mouseY = -(event.clientY / window.innerHeight) * 2 + 1 // موقعیت موس در محور Y
+
+          // تغییر مقدار هدف چرخش در محورهای مختلف
+          targetRotationX = (mouseY * Math.PI) / 32 // چرخش مدل در محور X
+          targetRotationZ = (mouseX * Math.PI) / 100 // چرخش مدل در محور Z
+
+          // حفظ مقدار ۹۰ درجه برای محور Y و تغییر کمی بر اساس حرکت موس
+          targetRotationY = Math.PI / 4 + (mouseX * Math.PI) / 45 // چرخش خیلی کم در محور Y
+        })
+
+        // هنگامی که موس از روی مدل خارج می‌شود، رویداد mousemove را غیرفعال می‌کنیم
+        model.addEventListener('mouseout', () => {
+          document.removeEventListener('mousemove', () => {})
+        })
+
+        // افزودن نور
+        const pointLight = new THREE.PointLight(0xffffff, 300)
+        pointLight.position.set(10, 10, 10)
+        scene.add(pointLight)
+      },
+      (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+      },
+      (error) => {
+        console.error('An error occurred while loading the OBJ model:', error)
+      },
+    )
+
+    // تنظیم موقعیت دوربین
+    camera.position.z = 5
+
+    // حلقه انیمیشن برای رندر کردن صحنه
+    function animate() {
+      requestAnimationFrame(animate)
+      renderer.render(scene, camera)
+    }
+    animate()
+
+    // به‌روز کردن رندرر هنگام تغییر سایز پنجره
+    window.addEventListener('resize', () => {
+      const newWidth = threeContainer.value.offsetWidth
+      const newHeight = threeContainer.value.offsetHeight
+      renderer.setSize(newWidth, newHeight)
+      camera.aspect = newWidth / newHeight
+      camera.updateProjectionMatrix() // به‌روزرسانی ماتریس دوربین
+    })
+  }
+  initThree()
   const VANTA = (await import('vanta/src/vanta.fog')).default
 
   vantaEffect = VANTA({
@@ -513,5 +656,38 @@ body {
   stroke: #96f0fc;
   stroke-width: 10;
   stroke-miterlimit: 10;
+}
+.section5 {
+  height: 100vh;
+  opacity: 0;
+  transition: opacity 1s ease-in-out;
+  display: flex;
+  align-items: center;
+  padding: 0 32px;
+  &__content {
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    color: #26337f;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 20px;
+    &__header {
+      display: flex;
+      flex-direction: column;
+      > span {
+        font-size: 44px;
+        font-weight: 700;
+      }
+    }
+    > span {
+      font-size: 24px;
+      font-weight: 400;
+    }
+  }
+}
+.three-container {
+  width: 40%;
+  height: 100%;
 }
 </style>
